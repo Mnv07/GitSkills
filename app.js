f97bc05f663368d7d5237d5f01082127509f1fee
“username” <mgoel7@asu.edu>
2024-11-03 09:17:23 -0700

    Enhance app: Add greetUser function for personalized greetings

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ function helloWorld() {
     console.log("Hello, world!");
 }
 helloWorld();
+
+function greetUser(name) {
+    console.log(`Hello, ${name}!`);  
+}
+
+helloWorld();  
+greetUser("Alice");  
